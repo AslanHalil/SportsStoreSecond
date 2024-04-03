ee3e8418c5c884152ec1298fd58b5dda916bb37f
AslanHalil <AslanCHalil@outlook.com>
2024-04-03 20:34:40 +0300

    Completing administration features.

---
+++ src/app/admin/orderTable.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  template: `<h3 style="padding-top: 10px">
+    Order table Placeholder
+  </h3>`
+})
+export class OrderTableComponent {
+}
