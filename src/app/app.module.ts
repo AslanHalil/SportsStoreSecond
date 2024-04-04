468f4528d0e483a72290519d33fbce592f1cb051
AslanHalil <AslanCHalil@outlook.com>
2024-04-04 15:47:40 +0300

    Making it a PWA.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {NgModule, isDevMode} from '@angular/core';
 import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {StoreModule} from "./store/store.module";
@@ -9,6 +9,7 @@ import {RouterModule} from "@angular/router";
 import {StoreFirstGuard} from "./storeFirst.guard";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {PlatformService} from './platform.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -36,7 +37,13 @@ import {PlatformService} from './platform.service';
     ], {
       initialNavigation: 'enabledBlocking'
     }),
-    BrowserAnimationsModule],
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })],
   providers: [StoreFirstGuard, provideClientHydration(), PlatformService],
   bootstrap: [AppComponent]
 })
--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -7,11 +7,13 @@ import {OrderRepository} from "./order.repository";
 import {RestDataSource} from "./rest.datasource";
 import {HttpClientModule} from "@angular/common/http";
 import {AuthService} from "./auth.service";
+import {ConnectionService} from "./connection.service";
 
 @NgModule({
   imports: [HttpClientModule],
   providers: [ProductRepository, StaticDataSource, Cart, Order,
-    OrderRepository, RestDataSource, AuthService]
+    OrderRepository, RestDataSource, AuthService,
+    ConnectionService]
 })
 export class ModelModule {
 }
