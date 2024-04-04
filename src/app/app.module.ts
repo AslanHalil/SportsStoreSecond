b43c8fb619980084b5cc4eed7077adeaa5e30858
AslanHalil <AslanCHalil@outlook.com>
2024-04-04 15:18:34 +0300

    Added hydration.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-
+import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {StoreModule} from "./store/store.module";
 import {StoreComponent} from "./store/store.component";
@@ -8,7 +7,7 @@ import {CheckoutComponent} from "./store/checkout.component";
 import {CartDetailComponent} from "./store/cartDetail.component";
 import {RouterModule} from "@angular/router";
 import {StoreFirstGuard} from "./storeFirst.guard";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -33,9 +32,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         canActivate: [StoreFirstGuard]
       },
       {path: "**", redirectTo: "/store"}
-    ]),
+    ], {
+      initialNavigation: 'enabledBlocking'
+    }),
     BrowserAnimationsModule],
-  providers: [StoreFirstGuard],
+  providers: [StoreFirstGuard, provideClientHydration()],
   bootstrap: [AppComponent]
 })
 export class AppModule {
